df743f2c52fe59767918b9f5d5519f1b52779f27
radika06 <Radzz900@gmail.com>
2022-10-04 17:55:00 +0700

    Pembaruan

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin/posts', function () {
+    return view('admin/posts');
+});
+
+Route::get('/admin', function () {
+    return view('admin/index');
+});
+Route::get('/login', function () {
+    return view('login/admin');
+});
+
+Route::get('/blog', function () {
+    return view('blog/index');
+});
+Route::get('/login', function () {
+    return view('admin/login');
+});
+
+Route::get('/about', function () {
+    return view('blog/about');
+});
+
+Route::get('/forgot', function () {
+    return view('admin/forgot');
+});
+
+Route::get('/register', function () {
+    return view('admin/register');
+});
+
+Route::get('/post', function () {
+    return view('blog/post');
+});
+
+Route::get('/contact', function () {
+    return view('blog/contact');
+});
+Route::resource('/admin/posts', PostController::class);
